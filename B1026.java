69a9c379b82d85dcca7d2e50d30103e76f19ea60
LeeJiSeon <j5119s@naver.com>
2019-05-28 00:09:36 +0900

    Update B1026.java
    성공한 코드

--- B1026.java
+++ B1026.java
@@ -9,34 +9,32 @@ public class B1026 {
 
         int[] A = new int[N];
         int[] B = new int[N];
+        int[] C = new int[N];
 
         for(int i = 0 ; i < N ; i++)
             A[i] = sc.nextInt();
-        for(int i = 0 ; i < N ; i++)
+        for(int i = 0 ; i < N ; i++) {
             B[i] = sc.nextInt();
+            C[i] = B[i];
+        }
 
-        HashMap<Integer, Integer> Bindex = new HashMap<>();
-        for(int i = 0 ; i < B.length ; i++)
-            Bindex.put(B[i], i);
 
         Arrays.sort(A);
-        ArrayList<Integer> list = new ArrayList<>();
-        for(int i = 0 ; i < N ; i++)
-            list.add(B[i]);
-        Collections.sort(list);
-        Collections.reverse(list);
-
-//        for(int i = 0 ; i < N ; i++)
-//            System.out.println(list.get(i));
         int[] result = new int[N];
-        int sum = 0;
-
-//        for(int i = 0 ; i < N ; i++)
-//            System.out.println(B[i]);
-
-        for(int i = 0 ; i < A.length ; i++){
-            result[Bindex.get(list.get(i))] = A[i];
+        for (int i = 0 ; i < A.length ; i++){
+            int max = C[0];
+            int index = 0;
+            for (int j = 1; j < B.length; j++) {
+                if (C[j] > max) {
+                    max = C[j];
+                    index = j;
+                }
+            }
+            result[index] = A[i];
+            C[index] = -1;
         }
+        
+        int sum = 0;
 
         for(int i = 0 ; i < N ; i++){
             sum += result[i] * B[i];
